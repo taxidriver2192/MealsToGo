6a05645494f5884125078c067c5d6830fb296779
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-12-03 15:07:29 +0100

    update data not done

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,5 +1,6 @@
 import { mocks } from "./mock";
 import camelize from "camelize";
+
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
@@ -9,7 +10,8 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
     resolve(mock);
   });
 };
-const restaurantsTransform = ({results = []}) => {
+
+export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
     return {
       ...restaurant,
@@ -17,13 +19,6 @@ const restaurantsTransform = ({results = []}) => {
       isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
     };
   });
+
   return camelize(mappedResults);
 };
-restaurantsRequest()
-  .then(restaurantsTransform)
-  .then((transformedResponse) => {
-    console.log(transformedResponse);
-  })
-  .catch((err) => {
-    console.log("error");
-  });
