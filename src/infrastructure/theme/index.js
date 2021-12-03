fabe4e1a1037d816a8e3ad4e0dd9592ce66878ed
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-12-03 12:12:45 +0100

    Big update ..

--- src/infrastructure/theme/index.js
+++ src/infrastructure/theme/index.js
@@ -1,5 +1,5 @@
 import { colors } from "./colors";
-import { space, lineHeights } from "./spacings";
+import { space, lineHeights } from "./spacing";
 import { sizes } from "./sizes";
 import { fonts, fontWeights, fontSizes } from "./fonts";
 
