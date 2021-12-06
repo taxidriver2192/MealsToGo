eaf3298b2e9b85b6d60aa9e26b9f916d27ec697b
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-12-06 13:15:21 +0100

    update try to ge seatch to work.. san francisco works in consloe log now :)

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,6 +1,8 @@
 import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
 
+import {locations} from "./locat"
+
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
