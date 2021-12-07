017d7fa7a4a5276a7a4515454018042c1b519bef
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-12-07 11:32:49 +0100

    now you can click on read more.

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-import { createStackNavigator } from "@react-navigation/stack";
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
+
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 
@@ -8,10 +13,19 @@ const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator headerMode="none">
+    <RestaurantStack.Navigator
+      headerMode="none"
+      screenOptions={{
+        ...TransitionPresets.ModalPresentationIOS,
+    }}
+    >
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
       />
     </RestaurantStack.Navigator>
   );
