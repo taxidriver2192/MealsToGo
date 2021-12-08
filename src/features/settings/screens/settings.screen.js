8713267afa764db5856bb4a497696ca083ecea7e
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-12-08 15:51:31 +0100

    new page in settings ther show liket resturangs

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,12 +1,12 @@
 import React, { useContext } from "react";
 import styled from "styled-components/native";
-import { List, Avatar } from "react-native-paper";
 
-import { SafeArea } from "../../../components/utility/safe-area.component";
-import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { List, Avatar } from "react-native-paper";
 
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -19,12 +19,13 @@ export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
   return (
     <SafeArea>
-        <AvatarContainer>
+      <AvatarContainer>
         <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
         <Spacer position="top" size="large">
-            <Text variant="label">{user.email}</Text>
+          <Text variant="label">{user.email}</Text>
         </Spacer>
       </AvatarContainer>
+
       <List.Section>
         <SettingsItem
           title="Favourites"
