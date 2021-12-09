e432ae9d427c08c2012488a54d6e5af6eb209f08
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-12-09 12:49:47 +0100

    error? not work...

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 import { List, Avatar } from "react-native-paper";
@@ -21,9 +20,7 @@ export const SettingsScreen = ({ navigation }) => {
   return (
     <SafeArea>
       <AvatarContainer>
-        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
-        </TouchableOpacity>
+        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
         </Spacer>
--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,8 +1,15 @@
 import camelize from "camelize";
 
-export const locationRequest = (searchTerm) => {};
+export const locationRequest = (searchTerm) => {
+  return fetch(
+    `http://localhost:5001/mealstogo-885bc/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
+  });
+};
 
 export const locationTransform = (result) => {
+  console.log(result);
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
