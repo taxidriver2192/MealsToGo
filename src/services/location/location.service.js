c17b5b362a8db923db33bf1bc39094db836d4456
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-12-09 16:50:37 +0100

    HOLDER FRI.. HAR KUN ERRORS!

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -8,8 +8,8 @@ export const locationRequest = (searchTerm) => {
 };
 
 export const locationTransform = (result) => {
-  console.log(result);
   const formattedResponse = camelize(result);
+  console.log(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
 
